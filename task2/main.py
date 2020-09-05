2a755bf84e44b9f5841b3f3dcbdf3ab8210df635
Chen Tzu-An <j6u06xjp4@gmail.com>
2020-09-05 21:10:13 +0800

    save_load @ rnn

--- task2/main.py
+++ task2/main.py
@@ -30,13 +30,17 @@ def run(args):
 	test_dataloader = dataloader('valid')
 	test_data = DataLoader(test_dataloader, batch_size = 1, shuffle = False, num_workers = 6, pin_memory = True)
 
-	model = Extractor()
+	rnn = Extractor()
 	if args.load != -1:
-		model.fc.load_state_dict(torch.load('models/linear_epoch{}.pkl'.format(args.load)))
-	model.cuda().float()
-	freeze_resnet50(model)
-	optimizer = torch.optim.Adam(filter(lambda param : param.requires_grad, model.parameters()), lr = 1e-6, weight_decay = 0.012)
-	# optimizer = torch.optim.Adam(model.parameters(), lr = 1e-8, weight_decay = 0.012)
+		checkpoint = torch.load('models/rnn_epoch{}.pkl'.format(args.load))
+		rnn.level1.load_state_dict(checkpoint['level1'])
+		rnn.level2.load_state_dict(checkpoint['level2'])
+		rnn.level3.load_state_dict(checkpoint['level3'])
+		rnn.predict.load_state_dict(checkpoint['predict'])
+
+	rnn.cuda().float()
+	freeze_resnet50(rnn)
+	optimizer = torch.optim.Adam(filter(lambda param : param.requires_grad, rnn.parameters()), lr = 1e-6, weight_decay = 0.012)
 
 	loss_func = nn.CrossEntropyLoss()
 	loss_func.cuda().float()
@@ -46,11 +50,11 @@ def run(args):
 
 	for epoch in range(args.load + 1, EPOCH):
 		total_loss = 0
-		model.train()
+		rnn.train()
 		hit = 0
 		for index, (video, label) in enumerate(tqdm(train_data, ncols = 80, desc = '[Training] epoch ({} / {})'.format(epoch, EPOCH))):
 			batch_video, batch_label = video.to(device), label.to(device)
-			prediction = model(batch_video)
+			prediction = rnn(batch_video)
 			hit += (torch.argmax(prediction).item() == batch_label.item())
 			loss = loss_func(prediction, batch_label)
 			total_loss += loss
@@ -63,12 +67,12 @@ def run(args):
 			f.write('epoch: {} / {}\ntrain_avg_loss: {:.4f}\ttrain_accuracy: {:.1f}%\n'.format(epoch, EPOCH, total_loss / len(train_data), hit / len(train_data) * 100))
 
 		total_loss = 0
-		model.eval()
+		rnn.eval()
 		hit = 0
 		with torch.no_grad():
 			for index, (video, label) in enumerate(tqdm(test_data, ncols = 80, desc = '[Testing] epoch ({} / {})'.format(epoch, EPOCH))):
 				batch_video, batch_label = video.to(device), label.to(device)
-				prediction = model(batch_video)
+				prediction = rnn(batch_video)
 				hit += (torch.argmax(prediction).item() == batch_label.item())
 				loss = loss_func(prediction, batch_label)
 				total_loss += loss
@@ -80,7 +84,11 @@ def run(args):
 
 		if not os.path.exists('models/'):
 			os.mkdir('models/')
-		torch.save(model.fc.state_dict(), 'models/linear_epoch{}.pkl'.format(epoch))
+		torch.save({'level1': rnn.level1.state_dict(),
+					'level2': rnn.level2.state_dict(),
+					'level3': rnn.level3.state_dict(),
+					'predict': rnn.predict.state_dict(),
+					}, 'models/rnn_epoch{}.pkl'.format(epoch))
 			
 if __name__ == '__main__':
 	args = _parse_args()
