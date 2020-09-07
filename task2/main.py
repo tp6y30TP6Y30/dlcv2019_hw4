fc2bd7d380d8eb100440639d26eb1b15d22187c1
Chen Tzu-An <j6u06xjp4@gmail.com>
2020-09-07 13:04:28 +0800

    try with rnn

--- task2/main.py
+++ task2/main.py
@@ -3,10 +3,11 @@ from dataloader import dataloader
 from torch.utils.data import DataLoader
 import numpy as np
 from tqdm import tqdm
-from models import Extractor
+from models import RNN
 import torch
 import torch.nn as nn
 import os
+import torch.nn.utils.rnn as rnn_utils
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -16,33 +17,38 @@ def _parse_args():
 	parser.add_argument('--load', type = int, default = -1)
 	return parser.parse_args()
 
-def freeze_resnet50(model):
-	for param in model.resnet50.parameters():
-		param.requires_grad = False
+def collate_fn(data):
+	data.sort(key = lambda x: len(x[0]), reverse = True)
+	feature, label = zip(*data)
+	length_list = [len(f) for f in feature]
+	feature = [torch.stack(f) for f in feature]
+	feature = rnn_utils.pad_sequence(feature, batch_first = True, padding_value = 0)
+	label = torch.stack(label, dim = 0)
+	return feature, label, length_list
+
+def cal_accuracy(batch_pred, batch_label):
+	hit, total = 0, batch_pred.size(0)
+	for index in range(0, batch_pred.size(0)):
+		hit += 1 if torch.argmax(batch_pred[index]) == batch_label[index] else 0
+	return hit, total
 
 def run(args):
 	torch.multiprocessing.freeze_support()
 	EPOCH = 30
-
+	batch_size = 16
 	train_dataloader = dataloader('train')
-	train_data = DataLoader(train_dataloader, batch_size = 1, shuffle = True, num_workers = 6, pin_memory = True)
+	train_data = DataLoader(train_dataloader, batch_size = batch_size, shuffle = True, num_workers = 6, pin_memory = True, collate_fn = collate_fn)
 
 	test_dataloader = dataloader('valid')
-	test_data = DataLoader(test_dataloader, batch_size = 1, shuffle = False, num_workers = 6, pin_memory = True)
+	test_data = DataLoader(test_dataloader, batch_size = batch_size, shuffle = False, num_workers = 6, pin_memory = True, collate_fn = collate_fn)
 
-	rnn = Extractor()
+	rnn = RNN()
 	if args.load != -1:
-		checkpoint = torch.load('models/rnn_epoch{}.pkl'.format(args.load))
-		rnn.level1.load_state_dict(checkpoint['level1'])
-		rnn.level2.load_state_dict(checkpoint['level2'])
-		rnn.level3.load_state_dict(checkpoint['level3'])
-		rnn.predict.load_state_dict(checkpoint['predict'])
-
+		rnn.load_state_dict(torch.load('models/rnn_epoch{}.pkl'.format(args.load)))
 	rnn.cuda().float()
-	freeze_resnet50(rnn)
-	optimizer = torch.optim.Adam(filter(lambda param : param.requires_grad, rnn.parameters()), lr = 1e-6, weight_decay = 0.012)
+	optimizer = torch.optim.Adam(rnn.parameters(), lr = 1e-5, weight_decay = 0.012)
 
-	loss_func = nn.CrossEntropyLoss()
+	loss_func = nn.CrossEntropyLoss(reduction = 'mean')
 	loss_func.cuda().float()
 
 	with open('loss_accuracy.txt', 'w' if args.load == -1 else 'a') as f:
@@ -51,44 +57,46 @@ def run(args):
 	for epoch in range(args.load + 1, EPOCH):
 		total_loss = 0
 		rnn.train()
-		hit = 0
-		for index, (video, label) in enumerate(tqdm(train_data, ncols = 80, desc = '[Training] epoch ({} / {})'.format(epoch, EPOCH))):
-			batch_video, batch_label = video.to(device), label.to(device)
-			prediction = rnn(batch_video)
-			hit += (torch.argmax(prediction).item() == batch_label.item())
+		all_hit = all_total = 0
+		for index, (feature, label, length_list) in enumerate(tqdm(train_data, ncols = 80, desc = '[Training] epoch ({} / {})'.format(epoch, EPOCH))):
+			batch_feature, batch_label, batch_length_list = feature.to(device), label.to(device), length_list
+			prediction = rnn(batch_feature, batch_length_list)
+			prediction = prediction.squeeze(1)
+			hit, total = cal_accuracy(prediction, batch_label)
+			all_hit += hit
+			all_total += total
 			loss = loss_func(prediction, batch_label)
 			total_loss += loss
 			loss.backward()
 			optimizer.step()
 			
 		print('epoch: {} / {}\ntrain_avg_loss: {:.4f}'.format(epoch, EPOCH, total_loss / len(train_data)))
-		print('train_accuracy: {:.1f}%'.format(hit / len(train_data) * 100))
+		print('train_accuracy: {:.1f}%'.format(all_hit / all_total * 100))
 		with open('loss_accuracy.txt', 'a') as f:
-			f.write('epoch: {} / {}\ntrain_avg_loss: {:.4f}\ttrain_accuracy: {:.1f}%\n'.format(epoch, EPOCH, total_loss / len(train_data), hit / len(train_data) * 100))
+			f.write('epoch: {} / {}\ntrain_avg_loss: {:.4f}\ttrain_accuracy: {:.1f}%\n'.format(epoch, EPOCH, total_loss / len(train_data), all_hit / all_total * 100))
 
 		total_loss = 0
 		rnn.eval()
-		hit = 0
+		all_hit = all_total = 0
 		with torch.no_grad():
-			for index, (video, label) in enumerate(tqdm(test_data, ncols = 80, desc = '[Testing] epoch ({} / {})'.format(epoch, EPOCH))):
-				batch_video, batch_label = video.to(device), label.to(device)
-				prediction = rnn(batch_video)
-				hit += (torch.argmax(prediction).item() == batch_label.item())
+			for index, (feature, label, length_list) in enumerate(tqdm(test_data, ncols = 80, desc = '[Testing] epoch ({} / {})'.format(epoch, EPOCH))):
+				batch_feature, batch_label, batch_length_list = feature.to(device), label.to(device), length_list
+				prediction = rnn(batch_feature, batch_length_list)
+				prediction = prediction.squeeze(1)
+				hit, total = cal_accuracy(prediction, batch_label)
+				all_hit += hit
+				all_total += total
 				loss = loss_func(prediction, batch_label)
 				total_loss += loss
 
 			print('epoch: {} / {}\ntest_avg_loss: {:.4f}'.format(epoch, EPOCH, total_loss / len(test_data)))
 			print('test_accuracy: {:.1f}%'.format(hit / len(test_data) * 100))
 			with open('loss_accuracy.txt', 'a') as f:
-				f.write('test_avg_loss: {:.4f}\ttest_accuracy: {:.1f}%\n'.format(total_loss / len(test_data), hit / len(test_data) * 100))
+				f.write('test_avg_loss: {:.4f}\ttest_accuracy: {:.1f}%\n'.format(total_loss / len(test_data), all_hit / all_total * 100))
 
 		if not os.path.exists('models/'):
 			os.mkdir('models/')
-		torch.save({'level1': rnn.level1.state_dict(),
-					'level2': rnn.level2.state_dict(),
-					'level3': rnn.level3.state_dict(),
-					'predict': rnn.predict.state_dict(),
-					}, 'models/rnn_epoch{}.pkl'.format(epoch))
+		torch.save(rnn.state_dict(), 'models/rnn_epoch{}.pkl'.format(epoch))
 			
 if __name__ == '__main__':
 	args = _parse_args()
--- task2/models.py
+++ task2/models.py
@@ -1,59 +1,23 @@
-import torchvision.models as models
 import torch
 import torch.nn as nn
+import torch.nn.utils.rnn as rnn_utils
 
-class Extractor(nn.Module):
+class RNN(nn.Module):
     def __init__(self, feature_size = 1000, hidden_size = 1000, output_size = 11):
-        super(Extractor, self).__init__()
-        # resnet50_output_size: (T, 1000)
-        self.resnet50 = models.resnet50(pretrained = True, progress = True)
-        self.hidden_size = hidden_size
-        self.hidden_init = self.init_hidden()
-        self.level1 = nn.Sequential(
-                            nn.Linear(feature_size + hidden_size, hidden_size),
-                            nn.ReLU(True),
-                      )
-        self.level2 = nn.Sequential(
-                            nn.Linear(feature_size + hidden_size, hidden_size),
-                            nn.ReLU(True),
-                      )
-        self.level3 = nn.Sequential(
-                            nn.Linear(feature_size + hidden_size, hidden_size),
-                            nn.ReLU(True),
-                      )
-        self.predict = nn.Sequential(
-                            nn.Linear(hidden_size, output_size),
-                            nn.Softmax(dim = 1),
-                      )
-        self.weights_init_uniform(self.level1)
-        self.weights_init_uniform(self.level2)
-        self.weights_init_uniform(self.level3)
-        self.weights_init_uniform(self.predict)
+        super(RNN, self).__init__()
+        self.rnn = nn.RNN(input_size = feature_size, hidden_size = hidden_size, num_layers = 2, batch_first = True, bidirectional = True)
+        self.fc = nn.Sequential(
+                        nn.Linear(feature_size + hidden_size, feature_size),
+                        nn.ReLU(True),
+                        nn.Linear(feature_size, output_size),
+                        nn.Softmax(dim = 2),
+                  )
 
-    def init_hidden(self):
-        return torch.zeros(1, self.hidden_size).cuda()
-
-    def weights_init_uniform(self, m):
-        classname = m.__class__.__name__
-        # for every Linear layer in a model..
-        if classname.find('Linear') != -1:
-            # apply a uniform distribution to the weights and a bias=0
-            m.weight.data.uniform_(0.0, 0.5)
-            m.bias.data.fill_(0)
-
-    def sample_seperate(self, feature):
-        return feature[0].unsqueeze(0), feature[int(feature.size(0) / 2)].unsqueeze(0), feature[-1].unsqueeze(0)
-
-    def combine(self, tensor1, tensor2):
-        return torch.cat((tensor1, tensor2), dim = 1)
-
-    def forward(self, input):
-        input = input.squeeze(0)
-        feature = self.resnet50(input)
-        feature_front, feature_mid, feature_tail = self.sample_seperate(feature)
-        feature = self.level1(self.combine(feature_front, self.hidden_init))
-        feature = self.level2(self.combine(feature_mid, feature))
-        feature = self.level3(self.combine(feature_tail, feature))
-        output = self.predict(feature)
-        return output
+    def forward(self, input, length_list): 
+        input_pack = rnn_utils.pack_padded_sequence(input, length_list, batch_first = True)
+        output, _ = self.rnn(input_pack)
+        output, _ = rnn_utils.pad_packed_sequence(output, batch_first = True)
+        output = self.fc(output)
+        pred = torch.mean(output, dim = 1, keepdim = True)
+        return pred
 
