86e878b2cfb24245460d05c66eda07c18be0a2f4
Chen Tzu-An <j6u06xjp4@gmail.com>
2020-09-10 00:35:32 +0800

    logsoftmax + nllloss

--- task2/main.py
+++ task2/main.py
@@ -17,8 +17,8 @@ def _parse_args():
 	parser.add_argument('--load', type = int, default = -1)
 	return parser.parse_args()
 
-def freeze_resnet50(model):
-	for param in model.resnet50.parameters():
+def freeze_pretrain(model):
+	for param in model.pretrain.parameters():
 		param.requires_grad = False
 
 def collate_fn(data):
@@ -32,61 +32,41 @@ def collate_fn(data):
 	return video, frame_size, label
 
 def cal_accuracy(batch_pred, batch_label):
-	hit, total = torch.sum(torch.argmax(batch_pred) == batch_label).item(), batch_pred.size(0)
+	hit, total = torch.sum(torch.argmax(batch_pred, dim = 1) == batch_label).item(), batch_pred.size(0)
 	return hit, total
 
 def run(args):
-	torch.multiprocessing.freeze_support()
-	EPOCH = 100
-	batch_size = 2
-	train_dataloader = dataloader('train')
-	train_data = DataLoader(train_dataloader, batch_size = batch_size, shuffle = True, num_workers = 6, pin_memory = True, collate_fn = collate_fn)
+	if args.mode == 'train':
+		torch.multiprocessing.freeze_support()
+		EPOCH = 100
+		batch_size = 1
+		train_dataloader = dataloader('train')
+		train_data = DataLoader(train_dataloader, batch_size = batch_size, shuffle = True, num_workers = 6, pin_memory = True, collate_fn = collate_fn)
 
-	test_dataloader = dataloader('valid')
-	test_data = DataLoader(test_dataloader, batch_size = batch_size, shuffle = False, num_workers = 6, pin_memory = True, collate_fn = collate_fn)
+		test_dataloader = dataloader('valid')
+		test_data = DataLoader(test_dataloader, batch_size = batch_size, shuffle = False, num_workers = 6, pin_memory = True, collate_fn = collate_fn)
 
-	model = LSTM()
-	if args.load != -1:
-		checkpoint = torch.load('models/model_epoch{}.pkl'.format(args.load))
-		model.lstm.load_state_dict(checkpoint['model_lstm'])
-		model.fc.load_state_dict(checkpoint['model_fc'])
+		model = LSTM()
+		if args.load != -1:
+			checkpoint = torch.load('models/model_epoch{}.pkl'.format(args.load))
+			model.lstm1.load_state_dict(checkpoint['model_lstm1'])
+			model.fc.load_state_dict(checkpoint['model_fc'])
 
-	freeze_resnet50(model)
-	model.cuda().float()
-	optimizer = torch.optim.Adam(filter(lambda param : param.requires_grad, model.parameters()), lr = 1e-7, weight_decay = 0.012)
+		freeze_pretrain(model)
+		model.cuda().float()
+		optimizer = torch.optim.Adam(filter(lambda param : param.requires_grad, model.parameters()), lr = 1e-5, weight_decay = 0.012)
 
-	loss_func = nn.CrossEntropyLoss(reduction = 'mean')
-	loss_func.cuda().float()
+		loss_func = nn.NLLLoss(reduction = 'mean')
+		loss_func.cuda().float()
 
-	with open('loss_accuracy.txt', 'w' if args.load == -1 else 'a') as f:
-		pass
+		with open('loss_accuracy.txt', 'w' if args.load == -1 else 'a') as f:
+			pass
 
-	for epoch in range(args.load + 1, EPOCH):
-		total_loss = 0
-		model.train()
-		all_hit = all_total = 0
-		for index, (video, frame_size, label) in enumerate(tqdm(train_data, ncols = 80, desc = '[Training] epoch ({} / {})'.format(epoch, EPOCH))):
-			batch_video, batch_frame_size, batch_label = video.to(device), frame_size.to(device), label.to(device)
-			prediction = model(batch_video, batch_frame_size)
-			prediction = prediction.squeeze(1)
-			hit, total = cal_accuracy(prediction, batch_label)
-			all_hit += hit
-			all_total += total
-			loss = loss_func(prediction, batch_label)
-			total_loss += loss
-			loss.backward()
-			optimizer.step()
-			
-		print('epoch: {} / {}\ntrain_avg_loss: {:.4f}'.format(epoch, EPOCH, total_loss / len(train_data)))
-		print('train_accuracy: {:.1f}%'.format(all_hit / all_total * 100))
-		with open('loss_accuracy.txt', 'a') as f:
-			f.write('epoch: {} / {}\ntrain_avg_loss: {:.4f}\ttrain_accuracy: {:.1f}%\n'.format(epoch, EPOCH, total_loss / len(train_data), all_hit / all_total * 100))
-
-		total_loss = 0
-		model.eval()
-		all_hit = all_total = 0
-		with torch.no_grad():
-			for index, (video, frame_size, label) in enumerate(tqdm(test_data, ncols = 80, desc = '[Testing] epoch ({} / {})'.format(epoch, EPOCH))):
+		for epoch in range(args.load + 1, EPOCH):
+			total_loss = 0
+			model.train()
+			all_hit = all_total = 0
+			for index, (video, frame_size, label) in enumerate(tqdm(train_data, ncols = 80, desc = '[Training] epoch ({} / {})'.format(epoch, EPOCH))):
 				batch_video, batch_frame_size, batch_label = video.to(device), frame_size.to(device), label.to(device)
 				prediction = model(batch_video, batch_frame_size)
 				prediction = prediction.squeeze(1)
@@ -95,17 +75,65 @@ def run(args):
 				all_total += total
 				loss = loss_func(prediction, batch_label)
 				total_loss += loss
-
-			print('epoch: {} / {}\ntest_avg_loss: {:.4f}'.format(epoch, EPOCH, total_loss / len(test_data)))
-			print('test_accuracy: {:.1f}%'.format(all_hit / all_total * 100))
+				optimizer.zero_grad()
+				loss.backward()
+				optimizer.step()
+				
+			print('epoch: {} / {}\ntrain_avg_loss: {:.4f}'.format(epoch, EPOCH, total_loss / len(train_data)))
+			print('train_accuracy: {:.1f}%'.format(all_hit / all_total * 100))
 			with open('loss_accuracy.txt', 'a') as f:
-				f.write('test_avg_loss: {:.4f}\ttest_accuracy: {:.1f}%\n'.format(total_loss / len(test_data), all_hit / all_total * 100))
+				f.write('epoch: {} / {}\ntrain_avg_loss: {:.4f}\ttrain_accuracy: {:.1f}%\n'.format(epoch, EPOCH, total_loss / len(train_data), all_hit / all_total * 100))
 
-		if not os.path.exists('models/'):
-			os.mkdir('models/')
-		torch.save({'model_lstm': model.lstm.state_dict(),
-					'model_fc': model.fc.state_dict(),
-				   }, 'models/model_epoch{}.pkl'.format(epoch))
+			total_loss = 0
+			model.eval()
+			all_hit = all_total = 0
+			with torch.no_grad():
+				for index, (video, frame_size, label) in enumerate(tqdm(test_data, ncols = 80, desc = '[Testing] epoch ({} / {})'.format(epoch, EPOCH))):
+					batch_video, batch_frame_size, batch_label = video.to(device), frame_size.to(device), label.to(device)
+					prediction = model(batch_video, batch_frame_size)
+					prediction = prediction.squeeze(1)
+					hit, total = cal_accuracy(prediction, batch_label)
+					all_hit += hit
+					all_total += total
+					loss = loss_func(prediction, batch_label)
+					total_loss += loss
+
+				print('epoch: {} / {}\ntest_avg_loss: {:.4f}'.format(epoch, EPOCH, total_loss / len(test_data)))
+				print('test_accuracy: {:.1f}%'.format(all_hit / all_total * 100))
+				with open('loss_accuracy.txt', 'a') as f:
+					f.write('test_avg_loss: {:.4f}\ttest_accuracy: {:.1f}%\n'.format(total_loss / len(test_data), all_hit / all_total * 100))
+
+			if not os.path.exists('models/'):
+				os.mkdir('models/')
+			torch.save({'model_lstm1': model.lstm1.state_dict(),
+						'model_fc': model.fc.state_dict(),
+					   }, 'models/model_epoch{}.pkl'.format(epoch))
+
+	elif args.mode == 'valid':
+		with open('predict.txt', 'w') as f:
+			pass
+		batch_size = 1
+		test_dataloader = dataloader('valid')
+		test_data = DataLoader(test_dataloader, batch_size = batch_size, shuffle = False, num_workers = 6, pin_memory = True, collate_fn = collate_fn)
+
+		model = LSTM()
+		checkpoint = torch.load('models/model_epoch{}.pkl'.format(args.load))
+		model.lstm1.load_state_dict(checkpoint['model_lstm1'])
+		model.fc.load_state_dict(checkpoint['model_fc'])
+		model.cuda().float()
+
+		model.eval()
+		all_hit = all_total = 0
+		with torch.no_grad():
+			for index, (video, frame_size, label) in enumerate(tqdm(test_data, ncols = 80, desc = '[Testing]')):
+				batch_video, batch_frame_size, batch_label = video.to(device), frame_size.to(device), label.to(device)
+				prediction = model(batch_video, batch_frame_size)
+				prediction = prediction.squeeze(1)
+				hit, total = cal_accuracy(prediction, batch_label)
+				all_hit += hit
+				all_total += total
+				with open('predict.txt', 'a') as f:
+					f.write('{}\n'.format(torch.argmax(prediction).item()))
 			
 if __name__ == '__main__':
 	args = _parse_args()
--- task2/models.py
+++ task2/models.py
@@ -2,29 +2,57 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 import torch.nn.utils.rnn as rnn_utils
+import torch.nn.init as init
 import torchvision.models as models
 
+def depadding(feature, frame_size):
+    # feature.shape: (B, T(padded), C)
+    batch_size, frame, feature_size = feature.size()
+    depadded_feature = []
+    for index, f in enumerate(feature):
+        depadded_feature.append(f[0:frame_size[index]])
+    return depadded_feature
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, input):
+        return input
+
 class LSTM(nn.Module):
-    def __init__(self, feature_size = 1000, hidden_size = 1000, output_size = 11):
+    def __init__(self, feature_size = 2048, hidden_size = 2048, output_size = 11):
         super(LSTM, self).__init__()
-        self.resnet50 = models.resnet50(pretrained = True, progress = True)
-        self.lstm = nn.LSTM(input_size = feature_size, hidden_size = hidden_size, num_layers = 5, batch_first = True, bidirectional = True)
+        self.pretrain = models.resnet50(pretrained = True, progress = True)
+        self.pretrain.fc = Identity()
+        self.lstm1 = nn.LSTM(input_size = feature_size, hidden_size = hidden_size, num_layers = 2, batch_first = True, bidirectional = True, dropout = 0.5)
         self.fc = nn.Sequential(
-                        nn.Linear(feature_size + hidden_size, feature_size),
+                        nn.Linear(hidden_size * 2, hidden_size),
                         nn.ReLU(True),
-                        nn.Linear(feature_size, output_size),
-                        nn.Softmax(dim = 2),
+                        nn.Linear(hidden_size, output_size),
+                        nn.LogSoftmax(dim = 1),
                   )
+        self._initial_weights()
+
+    def _initial_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    init.zeros_(m.bias)
 
     def forward(self, video, frame_size):
         video_size = video.size()[2:]
         video = video.view((-1, ) + video_size)
-        feature = self.resnet50(video)
+        feature = self.pretrain(video)
         feature = feature.view(frame_size.size(0), -1, feature.size(-1))
         feature_pack = rnn_utils.pack_padded_sequence(feature, frame_size, batch_first = True)
-        output, _ = self.lstm(feature_pack)
+        output, _ = self.lstm1(feature_pack)
         output, _ = rnn_utils.pad_packed_sequence(output, batch_first = True)
-        output = self.fc(output)
-        predict = torch.mean(output, dim = 1, keepdim = True)
+        # depadded_feature = depadding(output, frame_size)
+        predict = []
+        for index in range(len(output)):
+            predict.append(self.fc(output[index][frame_size[index] // 2].unsqueeze(0)))
+        predict = torch.stack(predict, dim = 0)
         return predict
 
